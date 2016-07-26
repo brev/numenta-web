066133d940dd578c10c294944d632e609dd9a950
Brev Patterson <me@brev.name>
2016-07-26 13:26:45 -0700

    Use IndexLink available from react-router for / links

--- numenta.com/components/TextLink/index.jsx
+++ numenta.com/components/TextLink/index.jsx
@@ -1,4 +1,4 @@
-import {Link} from 'react-router'
+import {IndexLink, Link} from 'react-router'
 import {prefixLink} from 'gatsby-helpers'  // eslint-disable-line import/no-unresolved, max-len
 import React from 'react'
 
@@ -7,16 +7,21 @@ import styles from './style.css'
 
 const TextLink = ({children, to}) => {
   const attrs = {
+    // default internal non-index link
     className: styles.textlink,
     to: prefixLink(to),
   }
-  let Node = Link  // default to internal link (react-router Link)
+  let Node = Link
 
-  // external link (browser location)
   if (to.match(/^.+:/)) {
+    // external link (browser location)
     Node = 'a'
     attrs.href = to
     delete attrs.to
+  }
+  else if (to === '/') {
+    // internal index main/home link
+    Node = IndexLink
   }
 
   return (
