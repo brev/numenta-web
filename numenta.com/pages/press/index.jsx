fcae62d5902a8e86b599cec1d774597cb1af3694
Brev Patterson <me@brev.name>
2016-08-16 14:21:46 -0700

    WEB-201 Initial re-split.

--- numenta.com/pages/press/index.jsx
+++ numenta.com/pages/press/index.jsx
@@ -6,6 +6,7 @@ import ListItem from '../../components/ListItem'
 import ListOrder from '../../components/ListOrder'
 import PostListItem from '../../components/PostListItem'
 import Section from '../../components/Section'
+import SubTitle from '../../components/SubTitle'
 
 
 /**
@@ -14,18 +15,35 @@ import Section from '../../components/Section'
 const PressPage = (props, {route}) => {
   const {pages} = route
   const posts = pages.filter(({file}) => (file.path.match(/^press\/.*\.md/)))
-  const items = posts.sort(sortDateDescend).map((post) => (
-    <ListItem key={post.file.stem}>
-      <PostListItem post={post} />
-    </ListItem>
-  ))
+  const pressLinks = posts
+    .filter(({data}) => (data.type === 'link'))
+    .sort(sortDateDescend).map((post) => (
+      <ListItem key={post.file.stem}>
+        <PostListItem post={post} />
+      </ListItem>
+    ))
+  const pressReleases = posts
+    .filter(({data}) => (data.type === 'post'))
+    .sort(sortDateDescend).map((post) => (
+      <ListItem key={post.file.stem}>
+        <PostListItem post={post} />
+      </ListItem>
+    ))
 
   return (
     <div>
       <Section headline={true} open={true} title="Press">
+
+        <SubTitle>Press Releases</SubTitle>
         <ListOrder copy={false}>
-          {items}
+          {pressReleases}
         </ListOrder>
+
+        <SubTitle>Press Links</SubTitle>
+        <ListOrder copy={false}>
+          {pressLinks}
+        </ListOrder>
+
       </Section>
     </div>
   )
