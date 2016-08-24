9b5070d0b766d0767a173a1c24d81db0768013b9
Brev Patterson <me@brev.name>
2016-08-24 10:25:16 -0700

    WEB-265 Catch markdown content links inside Markdown component, instead of in md.jsx wrapper (one level of abstraction in).

--- numenta.com/components/Markdown/index.jsx
+++ numenta.com/components/Markdown/index.jsx
@@ -1,3 +1,5 @@
+import catchLinks from 'catch-links'
+import {prefixLink} from 'gatsby-helpers'  // eslint-disable-line import/no-unresolved, max-len
 import React from 'react'
 
 import styles from './index.css'
@@ -6,14 +8,36 @@ import styles from './index.css'
 /**
  *
  */
-const Markdown = ({children}) => (
-  <div className={styles.markdown}>
-    {children}
-  </div>
-)
+class Markdown extends React.Component {
 
-Markdown.propTypes = {
-  children: React.PropTypes.any.isRequired,
+  static propTypes = {
+    children: React.PropTypes.any.isRequired,
+  }
+
+  static contextTypes = {
+    router: React.PropTypes.object,
+  }
+
+  componentDidMount() {
+    const {router} = this.context
+
+    // take over markdown local content links
+    catchLinks(this._markdown, (href) => router.push(prefixLink(href)))
+  }
+
+  render() {
+    const {children} = this.props
+
+    return (
+      <div
+        className={styles.markdown}
+        ref={(ref) => this._markdown = ref}
+      >
+        {children}
+      </div>
+    )
+  }
+
 }
 
 export default Markdown
