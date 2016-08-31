ce699289de83d93cb87f798a8169f2392327e393
Brev Patterson <me@brev.name>
2016-08-31 14:32:30 -0700

    WEB-296 fix content links right on top of each other

--- numenta.com/pages/applications/_Section.jsx
+++ numenta.com/pages/applications/_Section.jsx
@@ -311,13 +311,14 @@ const SectionApplications = (props, {config}) => {
         </div>
         <div className={styles.content}>
           <Paragraph>
-            One of our partners, Cortical.io, has used Numenta’s technology
-            to develop and commercialize Natural Language Processing
-            Solutions. By representing language with highly efficient semantic
-            fingerprints, Cortical.io has built the first semantic engine
-            that can analyze text in real time, in any language. Learn more
-            at {' '}
-            <TextLink to={links.out.cortical}>{links.out.cortical}.</TextLink>
+            One of our partners, {' '}
+            <TextLink to={links.out.cortical}>
+              Cortical.io,
+            </TextLink> {' '}
+            has used Numenta’s technology to develop and commercialize Natural
+            Language Processing Solutions. By representing language with highly
+            efficient semantic fingerprints, Cortical.io has built the first
+            semantic engine that can analyze text in real time, in any language.
           </Paragraph>
           <List marker="disc">
             <ListItem>
