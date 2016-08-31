d341e6cdf3887eef931895d674d64aac4e62b811
Brev Patterson <me@brev.name>
2016-08-31 13:02:08 -0700

    WEB-288 fix typo

--- numenta.com/pages/open-source-community/_Section.jsx
+++ numenta.com/pages/open-source-community/_Section.jsx
@@ -35,7 +35,7 @@ const SectionOpensource = (props, {config}) => {
         </Paragraph>
         <Paragraph>
           You’ll also find our implementation of HTM theory, called NuPIC
-          (Numenta Platform for Intelligence Computing), which is written in C++
+          (Numenta Platform for Intelligent Computing), which is written in C++
           and Python. Other implementations have been ported from NuPIC into
           other languages by community members.
         </Paragraph>
