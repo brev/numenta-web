e28a3d8fbb6d3d43b275a25aacba7382e5e87e90
Brev Patterson <me@brev.name>
2016-09-01 13:20:35 -0700

    Version bump. Make sure new icons are avail on staging path.

--- numenta.com/pages/_template.jsx
+++ numenta.com/pages/_template.jsx
@@ -80,7 +80,7 @@ class Template extends React.Component {
           const line = detail.replace(/[\[\]]/g, '')
           const [key, value] = line.split(/\$?=/)
           const clean = value.replace(/'/g, '')
-          details[key] = clean
+          details[key] = prefixLink(clean)
         })
       target.push(details)
     })
