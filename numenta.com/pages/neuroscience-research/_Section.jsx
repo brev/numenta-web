13f3fd9a46f6e1dfebcfd1116baa08efd4c839d9
Brev Patterson <me@brev.name>
2016-10-07 12:14:40 -0700

    WEB-287 update text per Jeff

--- numenta.com/pages/neuroscience-research/_Section.jsx
+++ numenta.com/pages/neuroscience-research/_Section.jsx
@@ -44,34 +44,57 @@ const SectionNeuroscience = (props, {config}) => {
             Reverse engineering the neocortex to figure out how the brain works
             is one of humanity's grand scientific challenges. We are fortunate
             to be living in an era when neuroscience techniques are advancing at
-            an amazing pace, giving rise to a wealth of data on the detailed
-            functionality of individual neurons all the way to large cortical
-            regions. Our team of researchers are constantly reading neuroscience
-            papers and studies that focus on various aspects of the brain. While
-            there is no shortage of neuroscientists that specialize in
-            particular areas, Numenta takes the unique approach of looking
-            across these studies and putting the pieces together to make a
-            working theory of the neocortex.
+            an amazing pace, giving rise to a wealth of data on everything from
+            synapses, to neurons, to entire cortical regions. Our research team
+            focuses on theory. We work with experimentalists and published
+            results to derive an understanding of what the neocortex does and
+            how the detailed architecture of the neocortex implements this. We
+            test our theories via simulation, mathematical analysis, and
+            experimental partnerships. While there are many experimental
+            neuroscience laboratories, Numenta is unique in its focus on
+            large-scale cortical theory and simulation.
           </Paragraph>
           <Paragraph>
-            We call this theory {' '}
-            <TextLink to={links.in.technology}>
-              Hierarchical Temporal Memory, (HTM),
+            Although there is much to do, we have made significant progress on
+            several key aspects of cortical theory. Our first focus was on how
+            the neocortex learns the structure in streams of sensory data. This
+            led to a comprehensive theory of why neurons in the neocortex have
+            thousands of synapses and active dendrites, and why they are
+            arranged in minicolumns as observed in the neocortex. We have taken
+            this theoretical advance and applied it to commercially valuable
+            problems in anomaly detection and prediction in streaming data. To
+            further that goal we created a {' '}
+            <TextLink to={links.in.nab}>
+              benchmark for anomaly detection in streaming data
             </TextLink> {' '}
-            a framework for both biological and machine intelligence. This
-            framework applies to everything the neocortex does – from vision to
-            language to motor and more.  Nothing is task-specific. Everything
-            operates on the same universal algorithms.
+            that for the first time provided a means for comparing the results
+            of different algorithms for anomaly detection. Our current research
+            focus is developing a comprehensive theory of how the neocortex
+            learns through movement — what is often called “sensorimotor”
+            learning.
           </Paragraph>
           <Paragraph>
-            We document our theory in the form of {' '}
+            We document our research in several ways, including {' '}
             <TextLink to={links.in.papers}>
-              research papers
+              peer-reviewed journal papers,
             </TextLink> {' '}
-            and a living book called {' '}
+            conference proceedings, research reports, {' '}
+            <TextLink to={links.in.events}>
+              invited talks,
+            </TextLink> {' '}
+            and a living book titled {' '}
             <TextLink to={links.in.bami}>
-              BAMI (Biological and Machine Intelligence).
-            </TextLink>
+              BAMI
+            </TextLink> {' '}
+            (for biological and machine intelligence). In addition, we place all
+            of our software, from our commercial grade applications to our daily
+            research commits, in our open source project, {' '}
+            <TextLink to={links.out.org}>
+              NuPIC.
+            </TextLink> {' '}
+            We strive to be completely open in everything we do. We welcome
+            collaborations with both neuroscientists and machine learning
+            researchers.
           </Paragraph>
         </div>
       </div>
