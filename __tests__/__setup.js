7b90e4e39ebe5280c6a9e811e2d315e4d14674e5
Brev Patterson <me@brev.name>
2016-12-19 13:43:13 -0800

    Lock timezone before running tests to make sure snapshots match up across servers, wherever they may be.

--- __tests__/__setup.js
+++ __tests__/__setup.js
@@ -3,8 +3,3 @@
 // Copyright © 2005—2017 Numenta <http://numenta.com>
 
 import JasmineExpect from 'jasmine-expect'  // eslint-disable-line
-import MockDate from 'mockdate'
-
-
-// lock down datetime+timezone for testing between dev/build boxes
-MockDate.set(1482182094 * 1000)
