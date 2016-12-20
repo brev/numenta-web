b2e9454516f1ac41cb0408be8f263ff2526f66b3
Brev Patterson <me@brev.name>
2016-12-20 14:08:31 -0800

    Add JUnit test result output, for Bamboo build interface.

--- __tests__/__setup.js
+++ __tests__/__setup.js
@@ -3,3 +3,20 @@
 // Copyright © 2005—2017 Numenta <http://numenta.com>
 
 import JasmineExpect from 'jasmine-expect'  // eslint-disable-line
+import {JUnitXmlReporter} from 'jasmine-reporters'
+
+const reporterJUnit = new JUnitXmlReporter({
+  // Jest runs many instances of Jasmine in parallel. Force distinct file output
+  // per test to avoid collisions.
+  consolidateAll: false,
+  filePrefix: 'jest-junit-',
+  savePath: 'coverage/junit-report/',
+})
+
+
+/**
+ * Initialize Jasmine testing environment context.
+ */
+
+// Add JUnit reporting to Jest+Jasmine
+jasmine.getEnv().addReporter(reporterJUnit)
